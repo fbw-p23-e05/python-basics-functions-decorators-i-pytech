12738aa777ad2d4f7dd9ae0666b48829bb3ea87d
ciwwan1980 <ajilqado@gmail.com>
2023-09-29 11:54:03 +0200

    retn result in decorator-call other func

--- task1.py
+++ task1.py
@@ -1,6 +1,35 @@
 
+def get_html_greeting():
+    return "Hello, World!"
+
+# print(get_html_greeting())
+
 def make_bold(func):
     def wrapper():
-        result = func()
-        return f"<strong>{result}</strong>"
+        result="<strong>" + func() + "</strong>"
+        print(result)
+        return result
     return wrapper
+
+# result= make_bold(get_html_greeting)
+# print(result)
+
+@make_bold
+def get_html_greeting():
+    return "Hello, World!"
+
+# print(get_html_greeting())
+
+get_html_greeting()
+
+# #  here how to use * and ** 
+# def print_arguments(*args, **kwargs):
+#     print("Positional arguments:")
+#     for arg in args:
+#         print(arg)
+
+#     print("Keyword arguments:")
+#     for key, value in kwargs.items():
+#         print(f"{key}: {value}", end="")
+
+# print_arguments("Hello", "World", name="John ", age=25, city=" Berlin")
