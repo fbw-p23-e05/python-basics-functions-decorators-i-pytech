608bb7e1f6f0715c6e9db953adebe7a59044cfd0
mkskh <maksym.kharchenko@dci-student.org>
2023-09-29 11:23:22 +0200

    added changes

--- task01.py
+++ task01.py
@@ -1,10 +1,10 @@
+def get_html_greeting():
+    return 'Hello, World!'
+
 def make_bold(func):
     def inner():
         print('<strong>'+ func()+ '</strong>')
     return inner
-
-def get_html_greeting():
-    return 'Hello, World!'
 
 dec = make_bold(get_html_greeting)
 dec()
